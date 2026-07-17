697bf321322fe380c52d589b30a6a3e995a6d1ec
arafatai388 <arafat.cseai@gmail.com>
2026-07-17 12:06:22 +0600

    practice pb 19

---
+++ problem_19.js
@@ -0,0 +1,11 @@
+//Using hasPermission, use the ! operator to print "Access Denied" if permission is not granted.
+
+const hasPermission=true;
+if(!hasPermission)
+{
+    console.log("Access Denied");
+}
+else
+{
+    console.log("Access Granted");
+}
