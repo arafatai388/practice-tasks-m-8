c45955b7e1ee861e6b5a1731f540435bf343a9d7
arafatai388 <arafat.cseai@gmail.com>
2026-07-17 12:12:43 +0600

    practice pb 20

---
+++ practice_20.js
@@ -0,0 +1,9 @@
+// Using isOnline, use the NOT operator to print "User is offline".
+
+const isOnline = true;
+
+if (!isOnline) {
+    console.log("User is offline");
+} else {
+    console.log("User is online");
+}
