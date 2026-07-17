67e4c27daafc3e91aa35b0054deae0ec4fbfd0f5
arafatai388 <arafat.cseai@gmail.com>
2026-07-17 11:12:33 +0600

    practice pb 10

---
+++ problem_10.js
@@ -0,0 +1,10 @@
+//Build a simple login system — print "Login successful" only if both username and password are correct.
+
+const username = "admin";
+const password = "12345";
+
+if (username === "admin" && password === "12345") {
+    console.log("Login successful");
+} else {
+    console.log("Invalid username or password");
+}
